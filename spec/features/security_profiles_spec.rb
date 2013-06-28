e1a4e8182c5a80b259445ffd1f6462caca005e38
wrwright <warren@thewrightangle.com>
2013-06-28 09:46:26 -0600

    need to add a home page

--- spec/features/security_profiles_spec.rb
+++ spec/features/security_profiles_spec.rb
@@ -7,11 +7,9 @@ feature 'the signin process' do
 
   scenario 'signing in with correct credentials' do
     visit '/login'
-    within("#session") do
-      fill_in 'Login', with: 'user@example.com'
-      fill_in 'Password', with: 'foobar'
-    end
-    click_link 'Sign in'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'foobar'
+    click_button 'Sign in'
     expect(page).to have_content 'Success'
   end
 end
