d34c30d4853e80cb3743d4b47a54ebcac88f3bea
wrwright <warren@thewrightangle.com>
2013-07-20 10:07:24 -0600

    modified to redirect to /login if hitting /group when not logged in

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,4 +1,6 @@
 class GroupController < ApplicationController
+  before_filter :authenticate_security_profile!
+
   def show
     render :index
   end
--- spec/features/groups_spec.rb
+++ spec/features/groups_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
-feature "display group details" do
+feature "group details" do
+  scenario "when not logged in redirect to login page" do
+    visit group_path
+    expect(current_path).to eq '/login'
+  end
+
   scenario "display details on a single group" do
     pending
   end
