512ff10602b8a351ffb31338a379eec90420b0ed
wrwright <warren@thewrightangle.com>
2013-08-02 01:59:10 -0600

    added has_many through relationships between groups and participants

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ActiveRecord::Base
-  has_many :memberships, dependent: :destroy
   has_many :events
   has_many :locations
+
+  has_many :memberships, dependent: :destroy
+  has_many :participants, through: :memberships
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,9 @@
 class Participant < ActiveRecord::Base
   has_many :security_profiles
-  has_many :memberships, dependent: :destroy
   has_many :attendances
+
+  has_many :memberships, dependent: :destroy
+  has_many :groups, through: :memberships
 end
 
 
